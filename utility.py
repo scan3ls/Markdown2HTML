1e928ed192dac6d8ef5e8f8f52a028a79960308b
scan3ls <scan3ls@gmail.com>
2020-12-18 08:39:41 -0600

    Testing print function failure

--- utility.py
+++ utility.py
@@ -12,4 +12,5 @@ def err_print(*args, **kwargs):
             kwargs - same as print **kwargs
     """
     from sys import stderr
-    print(*args, file=stderr, **kwargs)
+    # print(*args, file=stderr, **kwargs)
+    print(*args, **kwargs)
